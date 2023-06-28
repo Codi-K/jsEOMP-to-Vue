595f3a0833f39af60cef27ab4d4ee0a6d89c6ef7
Codi-K <codikader7@gmail.com>
2023-06-28 14:20:56 +0200

    vue installed

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <NavbarComp/>
+  <router-view/>
+</template>
+
+<script>
+
+import NavbarComp from '@/components/NavbarComp'
+
+export default {
+  components: {
+  NavbarComp
+ }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+</style>
